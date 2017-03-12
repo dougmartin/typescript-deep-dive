b9037824288cc8e15b92a7c03565d8c710337097
Doug Martin <doug@zoopdoop.com>
2017-03-12 15:15:05 -0400

    Step 4: refactor api to return links

--- src/server.ts
+++ src/server.ts
@@ -2,51 +2,120 @@
 import * as express from "express"
 import * as path from "path"
 import * as fs from "fs"
+import * as url from "url"
 
 // API
-interface Repo {
-  name: string,
-  folder: string
+interface APIIndex {
+  links: {
+    self: string
+    repos: string
+  }
 }
-
-type APIResult = Repo[]
+interface RepoIndex {
+  repos: Repo[]
+  links: {
+    self: string
+    parent: string
+  }
+}
+interface Repo {
+  name: string
+  slug: string
+  links: {
+    self: string
+    parent: string
+  }
+}
+type ErrorStatusCode = number
+type APIResult = APIIndex | RepoIndex | Repo | ErrorStatusCode
 
 class API {
-  baseRepoPath = path.resolve(path.join(__dirname, "../repos"))
+  baseRepoPath: string
 
-  repos(callback: (error:string|null, repos?:Repo[]) => void) {
+  constructor(private req:APIRequest) {
+    this.baseRepoPath = path.resolve(path.join(__dirname, "../repos"))
+  }
+
+  repos(callback: (error:string|null, repos?:RepoIndex|ErrorStatusCode) => void) {
     fs.readdir(this.baseRepoPath, (error, files) => {
       if (error) {
         callback(error.toString())
       }
       else {
-        callback(null, files.map<Repo>((filename) => {
-          const basename = path.basename(filename, ".git")
-          return {
-            name: `${basename.substr(0, 1).toUpperCase()}${basename.substr(1)}`,
-            folder: filename
+        callback(null, {
+          repos: files.map<Repo>(this.getRepoFromFilename.bind(this)),
+          links: {
+            self: this.req.apiUrl("/repos"),
+            parent: this.req.apiUrl("/")
           }
-        }))
+        })
       }
     })
+  }
+
+  repo(slug: string, callback: (error:string|null, repo?:Repo|ErrorStatusCode) => void) {
+    const repoFolder = `${path.basename(slug)}.git`
+    const repoPath = path.join(this.baseRepoPath, repoFolder)
+
+    fs.stat(repoPath, (error, stats) => {
+      if (error) {
+        if (error.code === "ENOENT") {
+          callback(`Unknown repo: ${slug}`, 404)
+        }
+        else {
+          callback(error.toString())
+        }
+      }
+      else {
+        callback(null, this.getRepoFromFilename(repoFolder))
+      }
+    })
+  }
+
+  private getRepoFromFilename(filename:string):Repo {
+    const slug = path.basename(filename, ".git")
+    return {
+      name: `${slug.substr(0, 1).toUpperCase()}${slug.substr(1)}`,
+      slug: slug,
+      links: {
+        self: this.req.apiUrl(`/repos/${slug}`),
+        parent: this.req.apiUrl("/repos")
+      }
+    }
   }
 }
 
 // SERVER CREATION
 const app = express()
 const port = process.env.PORT || 3000
-const api = new API()
+let api:API
 
 // MIDDLEWARE
 
-interface APIResponse extends express.Response {
-  api(error:string, result?: APIResult): express.Response
+interface APIRequest extends express.Request {
+  apiUrl(endPoint:string): string
 }
 
-app.use((req:express.Request, res:APIResponse, next:express.NextFunction) => {
+interface APIResponse extends express.Response {
+  api(error:string|null, result?: APIResult): express.Response
+}
+
+app.use((req:APIRequest, res:APIResponse, next:express.NextFunction) => {
+  api = new API(req)
+
+  req.apiUrl = (endPoint) => {
+    return url.format({
+      protocol: req.protocol,
+      hostname: req.hostname,
+      port: port,
+      pathname: `/api${endPoint}`
+    })
+  }
+
   res.api = (error, result) => {
     res.setHeader("Content-Type", "application/json")
     if (error) {
+      res.statusCode = typeof result === "number" ? result : 500
       return res.json({success: false, error: error})
     }
     return res.json({success: true, result: result})
@@ -60,8 +129,21 @@ app.get("/", (req:express.Request, res:express.Response) => {
   res.send("Hello world!")
 })
 
-app.get("/api/", (req:express.Request, res:APIResponse) => {
+app.get("/api", (req:APIRequest, res:APIResponse) => {
+  res.api(null, {
+    links: {
+      self: req.apiUrl("/"),
+      repos: req.apiUrl("/repos")
+    }
+  })
+})
+
+app.get("/api/repos", (req:APIRequest, res:APIResponse) => {
   api.repos(res.api)
+})
+
+app.get("/api/repos/:slug", (req:APIRequest, res:APIResponse) => {
+  api.repo(req.params.slug, res.api)
 })
 
 // SERVER STARTUP

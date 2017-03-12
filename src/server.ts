5642b5d6c13131806b1a953f875f0711c99900f5
Doug Martin <doug@zoopdoop.com>
2017-03-12 11:41:34 -0400

    Step 1: setup simple server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+// server.ts
+
+import * as express from "express"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello world!")
+})
+
+app.listen(port, () => {
+  console.log(`Started server on port ${port}`)
+})
